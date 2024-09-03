5f4469a2444ac93d5dadbf94c45315a96f1e2f5b
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2024-09-03 16:55:02 +0800

    add auto upload file script

---
+++ quic_data/create_directory.py
@@ -0,0 +1,144 @@
+import os
+import shutil
+
+##### VARIABLES SETTING #####
+# root_dir = "/Volumes/mollyT7/"
+root_dir = "/home/wmnlab/F/quic_data/"
+dates = ["2024-09-03"]
+exp_names = ["QUIC-bbr"]
+device_names = ["sm01", "sm02"]
+num_experiments = 4
+
+device_to_port = {"sm00": [5200, 5201], 
+                  "sm01": [5202, 5203], 
+                  "sm02": [5204, 5205],
+                  "sm03": [5206, 5207],
+                  "sm04": [5208, 5209],
+                  "sm05": [5210, 5211],
+                  "sm06": [5212, 5213],
+                  "sm07": [5214, 5215],
+                  "sm08": [5216, 5217],
+                  "sm09": [5218, 5219],
+                  }
+num_devices = len(device_names)
+quic_exp_names = [exp_name for exp_name in exp_names if "QUIC" in exp_name]
+udp_exp_names = [exp_name for exp_name in exp_names if "UDP" in exp_name]
+##### VARIABLES SETTING #####
+
+##### CREATING DERICTORIES #####
+def create_directory_structure(root_dir, exp_name, device_names, num_devices, num_experiments):
+    for device in device_names:
+            device_dir = os.path.join(root_dir, exp_name, device)
+            print(device_dir)
+            os.makedirs(device_dir, exist_ok=True)
+            for i in range(1, num_experiments + 1):
+                exp_dir = os.path.join(device_dir, f"#{i:02d}")
+                os.makedirs(exp_dir, exist_ok=True)
+                sub_dirs = ["raw", "middle", "data", "statistics"]
+                for sub_dir in sub_dirs:
+                    dir_path = os.path.join(exp_dir, sub_dir)
+                    os.makedirs(dir_path, exist_ok=True)
+
+for date in dates:
+    date_dir = root_dir + date
+    for exp_name in exp_names:
+        create_directory_structure(date_dir, exp_name, device_names, num_devices, num_experiments)
+print("Directory structure created successfully.")
+##### CREATING DERICTORIES #####
+        
+##### MOVING xml of MOBILEINSIGHT FILES #####
+for date in dates:
+    date_dir = os.path.join(root_dir, date)
+    mi2log_dir = os.path.join(root_dir, date, "mi2log_xml")
+    file_list = sorted(os.listdir(mi2log_dir))
+    for exp_name in exp_names:
+        for i in range(1, num_experiments + 1):
+            for device in device_names:
+                device_dir = os.path.join(date_dir, exp_name, device)
+                raw_dir = os.path.join(device_dir, f"#{i:02d}", "raw")
+                os.makedirs(raw_dir, exist_ok=True)
+                for file in file_list:
+                    if file.startswith("diag_log_" + device):
+                        src_file = os.path.join(mi2log_dir, file)
+                        file_list.remove(file)
+                        shutil.copy(src_file, raw_dir)
+                        break
+##### MOVING xml of MOBILEINSIGHT FILES #####
+
+##### MOVING QUIC SERVER qlog FILES #####
+for date in dates:
+    date_dir = os.path.join(root_dir, date)
+    for exp_name in quic_exp_names:
+        server_dir = os.path.join(root_dir, date, "server_qlog")
+        file_list = sorted([file for file in os.listdir(server_dir) if file.endswith(".qlog")])
+        for i in range(1, num_experiments+1):
+            for device in device_names:
+                device_dir = os.path.join(date_dir, exp_name, device)
+                raw_dir = os.path.join(device_dir, f"#{i:02d}", "raw")
+                os.makedirs(raw_dir, exist_ok=True)
+                for t in range(2):
+                    port_num = file_list[0].split("_")[3]
+                    port_num = int(port_num)
+                    if port_num in device_to_port[device]:
+                        src_file = os.path.join(server_dir, file_list[0])
+                        file_list.remove(file_list[0])
+                        shutil.move(src_file, raw_dir)
+##### MOVING QUIC SERVER qlog FILES #####
+
+##### MOVING QUIC CLIENT qlog FILES #####
+for date in dates:
+    date_dir = os.path.join(root_dir, date)
+    for client in device_names:
+        client_dir = os.path.join(root_dir, date, client)
+        file_list = sorted([file for file in os.listdir(client_dir) if file.endswith(".qlog")])
+        for exp_name in quic_exp_names:
+            for i in range(1, num_experiments+1):
+                    device_dir = os.path.join(date_dir, exp_name, client)
+                    raw_dir = os.path.join(device_dir, f"#{i:02d}", "raw")
+                    os.makedirs(raw_dir, exist_ok=True)
+                    for t in range(2):
+                        port_num = file_list[0].split("_")[3]
+                        port_num = int(port_num)
+                        if port_num in device_to_port[client]:
+                            src_file = os.path.join(client_dir, file_list[0])
+                            file_list.remove(file_list[0])
+                            shutil.move(src_file, raw_dir)
+##### MOVING QUIC CLIENT qlog FILES #####
+
+# ##### MOVING UDP SERVER pcap FILES #####
+# for date in dates:
+#     date_dir = os.path.join(root_dir, date)
+#     server_dir = os.path.join(root_dir, date, "server_pcap")
+#     file_list = sorted(os.listdir(server_dir))
+#     for exp_name in udp_exp_names:
+#         for i in range(1, num_experiments + 1):
+#             for device in device_names:
+#                 device_dir = os.path.join(date_dir, exp_name, device)
+#                 raw_dir = os.path.join(device_dir, f"#{i:02d}", "raw")
+#                 os.makedirs(raw_dir, exist_ok=True)
+#                 for file in file_list:
+#                     if file.startswith("server_pcap_BL_" + device):
+#                         src_file = os.path.join(server_dir, file)
+#                         file_list.remove(file)
+#                         shutil.move(src_file, raw_dir)
+#                         break
+# ##### MOVING UDP SERVER pcap FILES #####
+
+# ##### MOVING UDP CLIENT pcap FILES #####
+# for date in dates:
+#     date_dir = os.path.join(root_dir, date)
+#     for client in device_names:
+#         client_dir = os.path.join(root_dir, date, client, date, "client_pcap")
+#         file_list = sorted(os.listdir(client_dir))
+#         for exp_name in udp_exp_names:
+#             for i in range(1, num_experiments + 1):
+#                 device_dir = os.path.join(date_dir, exp_name, client)
+#                 raw_dir = os.path.join(device_dir, f"#{i:02d}", "raw")
+#                 os.makedirs(raw_dir, exist_ok=True)
+#                 for file in file_list:
+#                     if file.startswith("client_pcap_BL_" + client):
+#                         src_file = os.path.join(client_dir, file)
+#                         file_list.remove(file)
+#                         shutil.move(src_file, raw_dir)
+#                         break
+# ##### MOVING UDP CLIENT pcap FILES #####
---
+++ quic_data/upload_log.py
@@ -0,0 +1,147 @@
+"""
+TODO: 
+    1. Implement scp command.
+USAGE:
+    1. add -c to the command line for the client to execute
+"""
+import os
+import sys
+import shutil
+import argparse
+import subprocess
+
+##### VARIABLES SETTING #####
+# root_dir = "/Volumes/mollyT7/"
+root_dir = "/home/wmnlab/F/quic_data"
+
+target_date = "2024-09-03"
+exp_names = ["QUIC-bbr"]
+device_names = ["sm01", "sm02"]
+num_experiments = 4
+
+device_to_port = {"sm00": [5200, 5201], 
+                  "sm01": [5202, 5203], 
+                  "sm02": [5204, 5205],
+                  "sm03": [5206, 5207],
+                  "sm04": [5208, 5209],
+                  "sm05": [5210, 5211],
+                  "sm06": [5212, 5213],
+                  "sm07": [5214, 5215],
+                  "sm08": [5216, 5217],
+                  "sm09": [5218, 5219],}
+
+serial_to_device = {"R5CR20FDXHK":"sm00",
+                    "R5CR30P9Z8Y":"sm01",
+                    "R5CRA1GCHFV":"sm02",
+                    "R5CRA1JYYQJ":"sm03",
+                    "R5CRA1EV0XH":"sm04",
+                    "R5CRA1GBLAZ":"sm05",
+                    "R5CRA1ESYWM":"sm06",
+                    "R5CRA1ET22M":"sm07",
+                    "R5CRA1D23QK":"sm08",
+                    "R5CRA2EGJ5X":"sm09",
+                    "R5CRA1ET5KB":"sm10",
+                    "R5CRA1D2MRJ":"sm11",}
+
+qlog_phone_folder = os.path.join("/sdcard/experiment_log", target_date)
+sync_phone_folder = os.path.join("/sdcard/experiment_log", target_date, "sync")
+server_folder = os.path.join("/home/wmnlab/temp", target_date)
+dest_computer_folder = os.path.join("/home/wmnlab/experiment_log", target_date)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-h', "--host", help="server ip address", default="wmnlab@140.112.20.183")
+parser.add_argument('-s', "--server", action='store_true', help="For the server to upload files.")
+parser.add_argument('-c', "--client", action='store_true', help="For the client to upload files.")
+parser.add_argument('-p', "--password", type=str, help="password for execution")
+args = parser.parse_args()
+##### VARIABLES SETTING #####
+
+##### GLOBAL VARIABLE & FUNCTIONS #####
+# def run_scp(directory, destination, password, port=7777):
+#     """Runs the SCP command to copy a directory to a remote destination."""
+#     try:
+#         command = f"sshpass -p '{password}' scp -r -P {port} {directory} {destination}"
+#         subprocess.run(command, shell=True, check=True)
+#         print(f"Successfully copied {directory} to {destination} on port {port}.")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Failed to copy {directory} to {destination}. Error: {e}")
+##### GLOBAL VARIABLE & FUNCTIONS #####
+    
+##### CLIENT UPLOAD #####
+if args.client:
+    ##### READ DEVICES #####
+    # Read devices connected to computer
+    adb_devices_command = "adb devices"
+    output = os.popen(adb_devices_command).read()
+
+    # check the devices
+    lines = output.strip().split("\n")[1:]
+    serial_numbers = {}
+    unauthorized_serial_numbers = {}
+    for line in lines:
+        parts = line.split("\t")
+        if len(parts) >= 2:
+            serial_number, status = parts[:2]
+            if status == "device":
+                serial_numbers[serial_to_device[serial_number]] = serial_number
+            elif status == "unauthorized":
+                unauthorized_serial_numbers[serial_to_device[serial_number]] = serial_number
+    # sort 
+    serial_numbers = dict(sorted(serial_numbers.items()))
+    unauthorized_serial_numbers = dict(sorted(unauthorized_serial_numbers.items()))
+    print("Connected devices:")
+    print(serial_numbers)
+    print("Unauthorized devices:")
+    print(unauthorized_serial_numbers)
+    if len(unauthorized_serial_numbers) != 0:
+        print("Error: There's unauthorized devices.")
+        sys.exit(1)
+    ##### READ DEVICES #####
+
+    ##### UPLOAD FILES #####
+    # copy the files to the connected computer
+    def pull_files(files, serial, dest_path):
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
+        for filename in files:
+            adb_command = f"adb -s {serial} pull {filename} {dest_path}"
+            os.system(adb_command)
+
+    # copy exp logs to the computer
+    for dev, serial in serial_numbers.items():
+        adb_ls_command = f"adb -s {serial} shell ls {qlog_phone_folder}"
+        output = os.popen(adb_ls_command).read()
+        phone_files = output.split()
+        qlog_files = [filename for filename in phone_files if filename.startswith("log") and filename.endswith(".qlog")]
+        time_files = [filename for filename in phone_files if filename.startswith("time") and filename.endswith(".txt")]
+        tls_files = [filename for filename in phone_files if filename.startswith("tls_key") and filename.endswith(".log")]
+        pull_files(qlog_files, serial, os.join(dest_computer_folder, dev, "client_qlog"))
+        pull_files(time_files, serial, os.join(dest_computer_folder, dev, "time_file"))
+        pull_files(tls_files, serial, os.join(dest_computer_folder, dev, "tls_key"))
+
+    # copy sync files to the computer
+    for dev, serial in serial_numbers.items():
+        adb_ls_command = f"adb -s {serial} shell ls {sync_phone_folder}"
+        output = os.popen(adb_ls_command).read()
+        phone_files = output.split()
+        sync_files = [filename for filename in phone_files if filename.startswith("time_sync") and filename.endswith(".json")]
+        pull_files(sync_files, serial, dest_computer_folder)
+    ##### UPLOAD FILES #####
+
+    # ##### SCP FILES #####
+    # run_scp(dest_computer_folder, args.password, args.host+":"+root_dir)
+    # ##### SCP FILES #####
+##### CLIENT UPLOAD #####
+
+##### SERVER UPLOAD #####
+# elif args.server:
+    # ##### SCP FILES #####
+    # run_scp(os.join(server_folder, "server_qlog"), args.password, args.host+":"+root_dir)
+    # run_scp(os.join(server_folder, "time_file"), args.password, args.host+":"+root_dir)
+    # run_scp(os.join(server_folder, "tls_key"), args.password, args.host+":"+root_dir)
+    # ##### SCP FILES #####
+##### SERVER UPLOAD #####
+
+else:
+    print("Error: You must provide either -s or -c as an argument.")
+    sys.exit(1)
